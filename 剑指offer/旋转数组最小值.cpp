7ece97dba02d4ed0ca7016eb480ad7b696676154
CrowFea <wendaoliu40@outlook.com>
2019-09-04 21:29:56 +0800

    递归

---
+++ 剑指offer/旋转数组最小值.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+        if(rotateArray.size()==0)    return 0;
+        for(int i=0;i<rotateArray.size()-1;++i){
+            if(rotateArray[i]>rotateArray[i+1])    return rotateArray[i+1];
+        }
+        return rotateArray[0];
+    }
+};
