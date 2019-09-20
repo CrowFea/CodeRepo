2a0c048edb321a9327a038f87d387c87443cefa6
CrowFea <wendaoliu40@outlook.com>
2019-09-20 15:57:00 +0800

    树的一些算法

--- Training/TreeAlgorithm.cpp
+++ Training/TreeAlgorithm.cpp
@@ -232,3 +232,60 @@ public:
     }
 
 };
+
+
+/*
+请实现一个函数按照之字形打印二叉树，
+即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，
+其他行以此类推。
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int> > Print(TreeNode* pRoot) {
+        vector<vector<int> > res;
+        if(pRoot == NULL)   return res;
+        stack<TreeNode*> even,odd;
+        odd.push(pRoot);
+        bool flag=true;
+        while(!odd.empty()||!even.empty()){
+            vector<int> temp;
+            if(flag)
+            {
+                while(!odd.empty()){
+                TreeNode* p=odd.top();
+                odd.pop();
+                temp.push_back(p->val);
+                if(p->left)
+                    even.push(p->left);
+                if(p->right)
+                    even.push(p->right);
+                }
+                flag=false;
+            }
+            else
+            {
+                while(!even.empty()){
+                TreeNode* p=even.top();
+                even.pop();
+                temp.push_back(p->val);
+                if(p->right)
+                    odd.push(p->right);
+                if(p->left)
+                    odd.push(p->left);
+                }
+                flag=true;
+            }
+            res.push_back(temp);
+        }
+    
+        return res;
+    }
+};
