f3061cd1a401511f0c3911e48f0673e51f106b62
CrowFea <wendaoliu40@outlook.com>
2019-09-24 15:10:43 +0800

    各种排序算法

--- Training/Sort.cpp
+++ Training/Sort.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
 //===============================直接插入排序
-    void StarightInsertSort(vector<ElemType> nums){
+    void StarightInsertSort(vector<ElemType> &nums){
         int n=nums.size();
         int senti=0;    //使用一个哨兵
         for(int i=1;i<n;i++){
@@ -17,7 +17,7 @@ public:
     }
 
 //===============================二分插入排序
-    void BinaryInsertSort(vector<ElemType> nums){
+    void BinaryInsertSort(vector<ElemType> &nums){
         int n=nums.size();
         int senti=0;
         for(int i=1;i<n;++i){
@@ -36,27 +36,36 @@ public:
     }
 
 //===============================希尔排序 
-    void ShellSort(vector<ElemType> nums){
+    void ShellSort(vector<ElemType> &nums){
         int n=nums.size();
-        for(int step=n/2;step>0;step/=2){
-            for(int i=0;i<i+step;i+=step){
-                for(int j=0;j<i; ++j){
-                    if(nums[j]<nums[j+1])
+        for(int step=n/2;step>=1;step/=2){
+            for(int i=step+1;i<n;++i){
+                if(nums[i]<nums[i-step]){
+                    nums[0]=nums[i];
+                    for(int j=i-step;j>0 && nums[0]<nums[j];j-=step)    nums[j+dk]=nums[j];
+                    nums[j+step]=nums[0];
                 }
             }
         }
     }
 
-    void BubbleSort(vector<ElemType> nums){
-        for(int i=0;i<nums.size();++i){
+//===============================冒泡排序
+    void BubbleSort(vector<ElemType> &nums){
+        bool flag=false;
+        for(int i=0;i<nums.size()-1;++i){
+            flag=false;
             for(int j=1;j<nums[i].size();++j){
-
+                if(nums[j]<nums[j-1]){
+                    swap(nums[i],nums[j]);
+                    flag=true;
+                }
             }
+            if(flag==false) return;
         }
     }
 
 //===============================================================快排
-    int partition(vector<ElemType> nums,int low,int high){
+    int partition(vector<ElemType> &nums,int low,int high){
         ElemType senti=nums[low];
         while(low<high){
             while(low<high && nums[high]>=senti)    --high;
@@ -68,7 +77,7 @@ public:
         return low;
     }
 
-    void Quicksort(vector<ElemType> nums,int low=0,int high=nums.size()){
+    void Quicksort(vector<ElemType> &nums,int low=0,int high=nums.size()){
         if(low<high){
             int partpos=parition(nums,low,high);
             Quicksort(nums,low,partpos-1);
@@ -78,7 +87,7 @@ public:
     }
 
 //========================================选择排序
-    void SelectSort(vector<ElemType> nums){
+    void SelectSort(vector<ElemType> &nums){
         for(int i=0;i<nums.size()-1;++i){
             int minpos=i;
             for(int j=i+1;j<nums.size();++j){
@@ -87,6 +96,69 @@ public:
             if(minpos!=i)   swap(nums[i],nums[minpos]);
         }
     }
+
+//============================================================堆排序
+    void AdjustHeap(vector<ElemType>& nums,int k,int n){
+        ElemType senti=nums[k];
+        for(int i=2*k;i<n;i*=2){
+            if(i<n && nums[i]<nums[i+1])
+                ++i;
+            if(nums[i]<senti)   break;
+            else{
+                nums[k]=nums[i];
+                k=i;
+            }
+        }
+        nums[k]=senti;
+    }
+
+    void BuildHeap(vector<ElemType>& nums){
+        int n=nums.size();
+        for(int i=n/2;i>0;--i){
+            AdjustHeap(nums,i,n);
+        }
+    }
+
+    void HeapSort(vector<ElemType> &nums){
+        BuildHeap(nums);
+        for(int i=nums.size()-1;i>0;--i){
+            swap(nums[0],nums[i]);
+            AdjustHeap(nums,0,i-1);
+        }
+    }
+//向上调整
+    void AdjustHeapUp(vector<ElemType> &nums,int k){
+        ElemType senti=nums[k];
+        int i=k/2;
+        while(i>=0 && nums[i]<senti){
+            nums[k]=nums[i];
+            k=i; 
+            i=k/2;
+        }
+        nums[k]=nums[0];
+    }
+
+//============================================================归并排序
+    void Merge(vector<ElemType> &nums,int low,int mid,int high){
+        vector<ElemType> temp=nums;
+        for(int i=low,j=mid+1,k=low;i<mid && j<high;++k){
+            if(temp[i]>temp[j]) nums[k]=temp[j++];
+            if(temp[i]<temp[j]) nums[k]=temp[i++];
+        }
+        while(i<mid)    nums[k++]=temp[i++];
+        while(j<high)   nums[k++]=temp[j++];
+    }
+
+    void MergeSort(vector<ElemType> &nums,int low,int high){
+        if(low<high){
+            int mid=(low+high)/2;
+            MergeSort(nums,low,mid);
+            MergeSort(nums,mid+1,high);
+            Merge(nums,low,mid,high);
+        }
+    }
+
+
 };
 
 
